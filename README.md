1a209d527e04c14cb7e18fc959c1254439db905e
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2024-08-26 08:34:35 -0400

    basic discord fastapi scaffold

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# streaming-setup
-Streaming Setup of APIs for Discord, Twitch &amp; YouTube
+# 🎥 Streaming Setup
+
+🎮 Streaming Setup of APIs for Discord, Twitch & YouTube
+
+## 🗂️ File Structure
+
+📁 `discord/` directory
+- 📝 `main.py`: FastAPI code for Routes
+- 🐳 `Dockerfile`: Dockerize FastAPI
+- 📦 `requirements.txt`: Required packages for the API
+- 💻 `.env`: Credentials to our APIs
+
+## 🚀 Run with Docker
+
+You can use `docker compose up` at the root of the project and will see your API available at `http://localhost:8000` 🌐
+
---
+++ discord/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Response
+from fastapi.openapi.models import Info
+from fastapi.openapi.models import ExternalDocumentation
+from fastapi.responses import RedirectResponse, HTMLResponse
+import requests
+from dotenv import load_dotenv
+import os
+import uvicorn
+import pandas as pd
+
+
+# Load environment variables from the .env file
+load_dotenv()
+
+app = FastAPI(
+    title="Majestic Coding Server Discord API",
+    description="Discord Bot for Github Commits & YouTube Releases.",
+    version="1.0.0",
+    openapi_tags=[
+        {
+            "name": "Github",
+            "description": "Github Commits for Discord Message.",
+        },
+        {
+            "name": "YouTube",
+            "description": "Latest Releases for Discord Message.",
+        },
+    ],
+    info=Info(
+        title="Majestic Coding Server Discord API",
+        version="1.0.0",
+        description="Discord Bot for Github Commits & YouTube Releases.",
+        terms_of_service="https://discord-bot-majestic-coding.onrender.com/terms",
+        contact={
+            "name": "Matt Majestic",
+            "url": "https://www.youtube.com/@majesticcoding",
+        },
+        license={
+            "name": "Apache 2.0",
+            "url": "https://www.apache.org/licenses/LICENSE-2.0",
+        },
+    ),
+    external_docs=ExternalDocumentation(
+        description="Find more information here",
+        url="https://discord-bot-majestic-coding.onrender.com/docs",
+    ),
+)
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    """
+    Redirect to the documentation.
+    """
+    response = RedirectResponse(url='/docs')
+    return response
+
+if __name__ == "__odds__":
+    uvicorn.run(app, host="0.0.0.0", port=8885)
